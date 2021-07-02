71d5855805cae0e190097762db3e7628bc150e3b
xamidjonovk <77206354+xamidjonovk@users.noreply.github.com>
2021-07-02 23:01:36 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters, ConversationHandler
+from telegram import (
+    InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, ChatAction
+)
+# db = Database("foods.db")
